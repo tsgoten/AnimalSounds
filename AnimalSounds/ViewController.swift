e68fe5ccda2e8fe49e3dc6c1e9ca62164bb066e3
Tarang <tsgoten@gmail.com>
2019-01-29 12:13:50 -0500

    Initial Commit

---
+++ AnimalSounds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalSounds
+//
+//  Created by Tarang Srivastava on 1/29/19.
+//  Copyright © 2019 Tarang Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
