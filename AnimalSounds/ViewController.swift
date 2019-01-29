aedba4646c0fb63969d320ead8135a57bef754b9
Tarang <tsgoten@gmail.com>
2019-01-29 12:49:11 -0500

    added functions for the button pressed

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animalSoundsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundsLabel.text = "Meow!"
+    }
+    @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundsLabel.text = "Bark!"
+    }
+    @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundsLabel.text = "Moo!"
+    }
+    
+    
 }
 
